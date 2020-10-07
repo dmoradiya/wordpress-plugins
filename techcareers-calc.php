39082253522c2eba1793e14ed48dfc93fc6cf5a1
dhmec <dharmoradiya@gmail.com>
2020-10-07 14:37:37 -0600

    Initial commit

---
+++ techcareers-calc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:       TECHCareers Calculator
+ * Plugin URI:        https://Techcareers.ca
+ * Description:       a simple PHP calculator
+ * Version:           1.0
+ * Requires at least: 5
+ * Requires PHP:      7.4
+ * Author:            Dharmesh
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
